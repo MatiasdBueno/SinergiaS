aab885c844c45789fb7539a1ffc593cc2caab968
Matias Daniel Bueno <matiasbueno01@hotmail.com>
2022-11-28 16:00:43 -0300

    Proyecto

---
+++ modal.js
@@ -0,0 +1,74 @@
+/* MODAL 0 -------------- */
+let openModal = document.getElementById('openModal');
+let modalInfo = document.getElementById('modal');
+let closeModal = document.getElementById('close');
+
+openModal.onclick = function(){
+    modalInfo.style.visibility = "visible";
+}
+
+closeModal.onclick = function(){
+    modalInfo.style.visibility = "hidden";
+}
+/* MODAL 1 -------------- */
+let openModal1 = document.getElementById('openModal1');
+let modalInfo1 = document.getElementById('modal1');
+let closeModal1 = document.getElementById('close1');
+
+openModal1.onclick = function(){
+    modalInfo1.style.visibility = "visible";
+}
+
+closeModal1.onclick = function(){
+    modalInfo1.style.visibility = "hidden";
+}
+/* MODAL 2 -------------- */
+let openModal2 = document.getElementById('openModal2');
+let modalInfo2 = document.getElementById('modal2');
+let closeModal2 = document.getElementById('close2');
+
+openModal2.onclick = function(){
+    modalInfo2.style.visibility = "visible";
+}
+
+closeModal2.onclick = function(){
+    modalInfo2.style.visibility = "hidden";
+}
+/* MODAL 3 -------------- */
+let openModal3 = document.getElementById('openModal3');
+let modalInfo3 = document.getElementById('modal3');
+let closeModal3 = document.getElementById('close3');
+
+openModal3.onclick = function(){
+    modalInfo3.style.visibility = "visible";
+}
+
+closeModal3.onclick = function(){
+    modalInfo3.style.visibility = "hidden";
+}
+/* MODAL 4 -------------- */
+let openModal4 = document.getElementById('openModal4');
+let modalInfo4 = document.getElementById('modal4');
+let closeModal4 = document.getElementById('close4');
+
+openModal4.onclick = function(){
+    modalInfo4.style.visibility = "visible";
+}
+
+closeModal4.onclick = function(){
+    modalInfo4.style.visibility = "hidden";
+}
+
+/* MODAL 5 -------------- */
+let openModal5 = document.getElementById('openModal5');
+let modalInfo5 = document.getElementById('modal5');
+let closeModal5 = document.getElementById('close5');
+
+openModal5.onclick = function(){
+    modalInfo5.style.visibility = "visible";
+}
+
+closeModal5.onclick = function(){
+    modalInfo5.style.visibility = "hidden";
+}
+
---
+++ modal1.js
@@ -0,0 +1,157 @@
+/* MODAL 0 -------------- */
+let openModal = document.getElementById('openModal');
+let modalInfo = document.getElementById('modal');
+let closeModal = document.getElementById('close');
+
+openModal.onclick = function(){
+    modalInfo.style.visibility = "visible";
+}
+
+closeModal.onclick = function(){
+    modalInfo.style.visibility = "hidden";
+}
+/* MODAL 1 -------------- */
+let openModal1 = document.getElementById('openModal1');
+let modalInfo1 = document.getElementById('modal1');
+let closeModal1 = document.getElementById('close1');
+
+openModal1.onclick = function(){
+    modalInfo1.style.visibility = "visible";
+}
+
+closeModal1.onclick = function(){
+    modalInfo1.style.visibility = "hidden";
+}
+/* MODAL 2 -------------- */
+let openModal2 = document.getElementById('openModal2');
+let modalInfo2 = document.getElementById('modal2');
+let closeModal2 = document.getElementById('close2');
+
+openModal2.onclick = function(){
+    modalInfo2.style.visibility = "visible";
+}
+
+closeModal2.onclick = function(){
+    modalInfo2.style.visibility = "hidden";
+}
+/* MODAL 3 -------------- */
+let openModal3 = document.getElementById('openModal3');
+let modalInfo3 = document.getElementById('modal3');
+let closeModal3 = document.getElementById('close3');
+
+openModal3.onclick = function(){
+    modalInfo3.style.visibility = "visible";
+}
+
+closeModal3.onclick = function(){
+    modalInfo3.style.visibility = "hidden";
+}
+/* MODAL 4 -------------- */
+let openModal4 = document.getElementById('openModal4');
+let modalInfo4 = document.getElementById('modal4');
+let closeModal4 = document.getElementById('close4');
+
+openModal4.onclick = function(){
+    modalInfo4.style.visibility = "visible";
+}
+
+closeModal4.onclick = function(){
+    modalInfo4.style.visibility = "hidden";
+}
+
+/* MODAL 5 -------------- */
+let openModal5 = document.getElementById('openModal5');
+let modalInfo5 = document.getElementById('modal5');
+let closeModal5 = document.getElementById('close5');
+
+openModal5.onclick = function(){
+    modalInfo5.style.visibility = "visible";
+}
+
+closeModal5.onclick = function(){
+    modalInfo5.style.visibility = "hidden";
+}
+/* MODAL 6 -------------- */
+let openModal6 = document.getElementById('openModal6');
+let modalInfo6 = document.getElementById('modal6');
+let closeModal6 = document.getElementById('close6');
+
+openModal6.onclick = function(){
+    modalInfo6.style.visibility = "visible";
+}
+
+closeModal6.onclick = function(){
+    modalInfo6.style.visibility = "hidden";
+}
+/* MODAL 7 -------------- */
+let openModal7 = document.getElementById('openModal7');
+let modalInfo7 = document.getElementById('modal7');
+let closeModal7 = document.getElementById('close7');
+
+openModal7.onclick = function(){
+    modalInfo7.style.visibility = "visible";
+}
+
+closeModal7.onclick = function(){
+    modalInfo7.style.visibility = "hidden";
+}
+/* MODAL 8 -------------- */
+let openModal8 = document.getElementById('openModal8');
+let modalInfo8 = document.getElementById('modal8');
+let closeModal8 = document.getElementById('close8');
+
+openModal8.onclick = function(){
+    modalInfo8.style.visibility = "visible";
+}
+
+closeModal8.onclick = function(){
+    modalInfo8.style.visibility = "hidden";
+}
+/* MODAL 9 -------------- */
+let openModal9 = document.getElementById('openModal9');
+let modalInfo9 = document.getElementById('modal9');
+let closeModal9 = document.getElementById('close9');
+
+openModal9.onclick = function(){
+    modalInfo9.style.visibility = "visible";
+}
+
+closeModal9.onclick = function(){
+    modalInfo9.style.visibility = "hidden";
+}
+/* MODAL 10 -------------- */
+let openModal10 = document.getElementById('openModal10');
+let modalInfo10 = document.getElementById('modal10');
+let closeModal10 = document.getElementById('close10');
+
+openModal10.onclick = function(){
+    modalInfo10.style.visibility = "visible";
+}
+
+closeModal10.onclick = function(){
+    modalInfo10.style.visibility = "hidden";
+}
+/* MODAL 11 -------------- */
+let openModal11 = document.getElementById('openModal11');
+let modalInfo11 = document.getElementById('modal11');
+let closeModal11 = document.getElementById('close11');
+
+openModal11.onclick = function(){
+    modalInfo11.style.visibility = "visible";
+}
+
+closeModal11.onclick = function(){
+    modalInfo11.style.visibility = "hidden";
+}
+/* MODAL 12 -------------- */
+let openModal12 = document.getElementById('openModal12');
+let modalInfo12 = document.getElementById('modal12');
+let closeModal12 = document.getElementById('close12');
+
+openModal12.onclick = function(){
+    modalInfo12.style.visibility = "visible";
+}
+
+closeModal12.onclick = function(){
+    modalInfo12.style.visibility = "hidden";
+}
